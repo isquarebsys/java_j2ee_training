d2159c6d96a5ce27f0a61b90effad7f9e9de274a
isquarebsys <isquarebsys@gmail.com>
2017-07-15 13:44:37 +0530

    Form handling sample added

---
+++ Spring4Web/src/controllers/StudentController.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import to.Student;
+
+@Controller
+public class StudentController {
+   @RequestMapping(value = "/add/student", method = RequestMethod.GET)
+   public ModelAndView student() {
+      return new ModelAndView("add_student", "command", new Student());
+   }
+   @RequestMapping(value = "/add/result", method = RequestMethod.POST)
+      public String addStudent(@ModelAttribute("SpringWeb")Student student,ModelMap model) {
+      model.addAttribute("name", student.getName());
+      model.addAttribute("age", student.getAge());
+      model.addAttribute("id", student.getId());
+      return "student_added";
+   }
+}
