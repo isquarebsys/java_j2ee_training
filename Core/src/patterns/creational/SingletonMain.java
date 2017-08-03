d04b614a0e096aa8c14dc453e36a6cbf78fb0cc1
isquarebsys <isquarebsys@gmail.com>
2017-08-03 13:36:57 +0530

    Singleton classes and gitignore

---
+++ Core/src/patterns/creational/SingletonMain.java
@@ -0,0 +1,10 @@
+package patterns.creational;
+
+public class SingletonMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Singleton singleton=Singleton.getInstance();
+	}
+
+}
--- Spring4Web/src/controllers/IndexHtmlController.java
+++ Spring4Web/src/controllers/IndexHtmlController.java
@@ -2,11 +2,14 @@ package controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexHtmlController {
-	 
+	
     @RequestMapping("/index")
     public String index(ModelMap model) {
         model.addAttribute("greeting", "index.html from Spring 4MVC");
