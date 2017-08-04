c065b19640a5c281a00a8e3a598f6169ecabd91a
isquarebsys <isquarebsys@gmail.com>
2017-08-04 19:46:55 +0530

    String utilities

--- Core/src/util/StringMain.java
+++ Core/src/util/StringMain.java
@@ -13,6 +13,30 @@ public class StringMain {
 		System.out.println("SB is "+sb.toString());
 		sb.delete(0, sb.length());
 		System.out.println("SB is "+sb.toString());
+		
+		int s11=2;
+		System.out.println("S1: "+s11);
+		s11=3;
+		System.out.println("S1: "+s11);
+		String s2="three";
+		String s3=s11+s2;
+		System.out.println("S3: "+s3);		
+		
+		StringBuffer sb1=new StringBuffer();
+		sb1.append(s11);
+		System.out.println("String Buffer: "+sb.toString());
+		sb1.append(s2);
+		System.out.println("String Buffer: "+sb.toString());
+		sb1.append(s11);
+		System.out.println("String buffer: "+sb.toString());
+		System.out.println("Index of 3: "+sb.indexOf("3"));
+		
+		String withUnderScore="Akash_V";
+		String[] splits=withUnderScore.split("_");
+		for(int i=0;i<splits.length;i++){
+			System.out.println("Split strings: "+splits[i]);
+		}		
+		
 	}
 
 }
