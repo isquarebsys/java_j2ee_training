2a5c74f93a7af80a0b2b48a2815977ca89ce8bbe
isquarebsys <isquarebsys@gmail.com>
2017-08-31 19:10:08 +0530

    Jersey Addition

---
+++ AngularJs/src/jersey/HelloWorld.java
@@ -0,0 +1,23 @@
+package jersey;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.*;
+
+@Path("hello")
+public class HelloWorld {
+
+@GET
+@Produces({MediaType.APPLICATION_JSON})
+   public Message getHello() {
+	Message m = new Message();
+    m.setDate(new Date());
+    m.setFirstName("Vijay");
+    m.setLastName("P");
+    m.setText("Hello World!");
+    return m;
+   }
+}
