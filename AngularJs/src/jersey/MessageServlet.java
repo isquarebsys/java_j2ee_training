505545fb0cbc2c7d178cd443d2ba1af7c1b5fb26
isquarebsys <isquarebsys@gmail.com>
2017-09-01 09:56:43 +0530

    Angular to call json of Servlet

--- AngularJs/src/jersey/MessageServlet.java
+++ AngularJs/src/jersey/MessageServlet.java
@@ -6,18 +6,22 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-@Path("hello")
-public class HelloWorld {
-
+@Path("message")
+public class MessageServlet {
+	private final String TAG="HelloWorld";
 @GET
 @Produces({MediaType.APPLICATION_JSON})
    public Message getHello() {
+	Logger.getLogger(TAG).log(Level.INFO, "getHello:Start");
 	Message m = new Message();
     m.setDate(new Date());
     m.setFirstName("Vijay");
     m.setLastName("P");
     m.setText("Hello World!");
+    Logger.getLogger(TAG).log(Level.INFO, "getHello:End");
     return m;
    }
 }
