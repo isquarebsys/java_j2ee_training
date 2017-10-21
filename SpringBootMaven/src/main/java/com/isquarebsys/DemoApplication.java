07c5e209e637957fc1b6a7d8e92b923b0d9f98cb
isquarebsys <isquarebsys@gmail.com>
2017-10-21 18:00:01 +0530

    JPA Updates

--- SpringBootMaven/src/main/java/com/isquarebsys/DemoApplication.java
+++ SpringBootMaven/src/main/java/com/isquarebsys/DemoApplication.java
@@ -1,8 +1,13 @@
 package com.isquarebsys;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+
+@EnableAutoConfiguration
+@Configuration
 @SpringBootApplication
 public class DemoApplication {
 
