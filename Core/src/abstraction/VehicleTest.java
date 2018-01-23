4731eb68d1aac1816f7013967afca18ccbe86152
isquarebsys <isquarebsys@gmail.com>
2018-01-23 12:14:48 +0530

    Spring boot updates

--- Core/src/abstraction/VehicleTest.java
+++ Core/src/abstraction/VehicleTest.java
@@ -5,7 +5,7 @@ public class VehicleTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		Vehicle vehicle=new Vehicle();
-		TwoWheeler twoWheeler=new TwoWheeler();
+		Vehicle twoWheeler=new TwoWheeler();
 		System.out.println("No of wheels: "+twoWheeler.noOfWheels());
 		
 
