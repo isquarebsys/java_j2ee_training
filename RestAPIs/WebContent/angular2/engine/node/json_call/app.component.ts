49a13e091593af19faa56d31565238837fd87773
isquarebsys <isquarebsys@gmail.com>
2018-01-24 15:47:36 +0530

    Angular 2 updates

---
+++ RestAPIs/WebContent/angular2/engine/node/json_call/app.component.ts
@@ -0,0 +1,30 @@
+import { Component,OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+	results: string;
+  title = 'Welcome to Angular 2';
+   // Inject HttpClient into your component or service.
+  constructor(private http: HttpClient) {}
+  heroes = [
+	new Hero(1, 'Windstorm'),
+	new Hero(20, 'Tornado')
+	];
+	myHero = this.heroes[0];
+	
+	ngOnInit(): void {
+    // Make the HTTP request:
+    this.http.get('http://localhost:8080/RestAPIs/jersey/message').subscribe(data => {
+      // Read the result field from the JSON response.
+      this.results = data['firstName'];
+	  //console.log("results: "+results);
+    });
+  }
+}
