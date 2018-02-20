ba79da81d79a05cfe597ce2c24989c5906248b1c
isquarebsys <isquarebsys@gmail.com>
2018-02-20 15:55:54 +0530

    Updates

--- RestAPIs/WebContent/angular2/engine/node/vendor/app.component.ts
+++ RestAPIs/WebContent/angular2/engine/node/vendor/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Vendor Portal';
+  title = 'Product List';
 }
--- RestAPIs/WebContent/angular2/engine/node/vendor/product-list/product-list.component.ts
+++ RestAPIs/WebContent/angular2/engine/node/vendor/product-list/product-list.component.ts
@@ -6,29 +6,49 @@ import { ProductService } from '../product.service';
   selector: 'app-product-list',
   template: `
   <!-- this is the new syntax for ng-repeat -->
-	<div class="table-responsive" id="form_details" align="center">
-		<table class="table-responsive" border="1">
-			<tr>
-				<td>Product Id</td>
-				<td>Product Name</td>
-				<td>Product Cost</td>
-			</tr>
-			<tr *ngFor="let product of products">
-				<td (click)="selectProduct(product)">{{product.id}} </td>
-				<td (click)="selectProduct(product)">{{product.name}} </td>
-				<td (click)="selectProduct(product)">{{product.cost}} </td>
-			</tr>
-		</table>
-	</div>
+  <div class="jumbotron" style="background-color:#FFFFFF;">
+		<div class="table-responsive" id="form_details" align="center">
+			<table class="table table-responsive table-bordered" align="center">
+				<tr>
+					<td>Product Id</td>
+					<td>Product Name</td>
+					<td>Product Cost</td>
+				</tr>
+				<tr *ngFor="let product of products">
+					<td (click)="selectProduct(product)">{{product.id}} </td>
+					<td (click)="selectProduct(product)">{{product.name}} </td>
+					<td (click)="selectProduct(product)">{{product.cost}} </td>
+				</tr>
+			</table>
 
-
-  <section *ngIf="selectedProduct">
-    <h2>You selected:  {{selectedProduct.id}}</h2>
-    <h3>Description</h3>
-    <p>
-       {{selectedProduct.name}} {{selectedProduct.cost}}
-    </p>
-  </section>
+			<form action="#" *ngIf="selectedProduct">
+			  <div class="form-group">
+				<label for="product_id">Product ID:</label>
+				<input type="text" class="form-control" id="product_id" value="{{selectedProduct.id}}">
+			  </div>
+			  <div class="form-group">
+				<label for="product_name">Product Name:</label>
+				<input type="text" class="form-control" id="product_name" value="{{selectedProduct.name}}">
+			  </div>
+			  <div class="form-group">
+				<label for="product_cost">Product Cost:</label>
+				<input type="text" class="form-control" id="product_cost" value="{{selectedProduct.cost}}">
+			  </div>
+			  <button type="submit" class="btn btn-default">Submit</button>
+			</form>
+			
+			<!--
+			<div class="well">
+			  <section >
+				<p>
+				   <li>Product Name: {{selectedProduct.name}} 
+				   <li>Product Cost: {{selectedProduct.cost}}
+				</p>
+			  </section>			
+		  </div>
+		  -->
+		</div>
+  </div>
   `
 })
 export class ProductListComponent implements OnInit {
