8b897ab5d0228213f557c52f9f22ab45b7c4b582
isquarebsys <isquarebsys@gmail.com>
2018-02-20 15:15:04 +0530

    Updates

--- RestAPIs/WebContent/angular2/engine/node/vendor/app.module.ts
+++ RestAPIs/WebContent/angular2/engine/node/vendor/app.module.ts
@@ -4,11 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-//import { PeopleListComponent } from './people-list/people-list.component';
-//import { PeopleService } from './people.service';
 //import { VendorService } from './vendor.service';
 import { ProductService } from './product.service';
-//import { PersonDetailsComponent } from './person-details/person-details.component';
 
 import { appRouterModule } from "./app.routes";
 //import { VendorListComponent } from './vendor-list/vendor-list.component';
