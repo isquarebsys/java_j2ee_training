ef99d82b5d20dabdeed9e8803aa36d9e85d00ded
isquarebsys <isquarebsys@gmail.com>
2018-03-30 15:49:49 +0530

    API Gateway using Spring and Zuul

--- SpringBootMaven/src/main/java/com/isquarebsys/rest/EmployeeController.java
+++ SpringBootMaven/src/main/java/com/isquarebsys/rest/EmployeeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EmployeeController
 {
-    @RequestMapping("/employees")
+    @RequestMapping("/springboot/employees")
     public List<Employee> getEmployees()
     {
         List<Employee> employeesList = new ArrayList<Employee>();
