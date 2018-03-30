be44fecceda428c9fcf2365b9c2d4eb9b287a36e
isquarebsys <isquarebsys@gmail.com>
2018-03-30 17:54:53 +0530

    API

--- SpringZuul/src/main/java/spring/GatewayApplication.java
+++ SpringZuul/src/main/java/spring/GatewayApplication.java
@@ -3,6 +3,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+
 import filters.SimpleFilter;
 
 /**
@@ -10,6 +11,7 @@ import filters.SimpleFilter;
  * @author user
  *
  */
+
 @EnableZuulProxy
 @SpringBootApplication
 public class GatewayApplication {
