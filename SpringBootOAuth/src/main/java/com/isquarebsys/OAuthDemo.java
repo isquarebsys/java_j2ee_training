7a63d718561d90157f1404175086925d3e30167a
isquarebsys <isquarebsys@gmail.com>
2018-04-26 14:30:51 +0530

    OAuth example

--- SpringBootOAuth/src/main/java/com/isquarebsys/OAuthDemo.java
+++ SpringBootOAuth/src/main/java/com/isquarebsys/OAuthDemo.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2012-2015 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.isquarebsys;
 
 import java.security.Principal;
@@ -52,6 +37,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.filter.CompositeFilter;
 
+/**
+ * Reference: https://spring.io/guides/tutorials/spring-boot-oauth2
+ * @author user
+ *
+ */
 @SpringBootApplication
 @RestController
 @EnableOAuth2Client
