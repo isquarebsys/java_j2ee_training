e084abb497d13f3b1641e07b91835c4fb31f1e57
isquarebsys <isquarebsys@gmail.com>
2018-05-12 12:09:53 +0530

    JSP Checks

--- SpringBootMaven/src/main/java/controllers/IndexController.java
+++ SpringBootMaven/src/main/java/controllers/IndexController.java
@@ -5,14 +5,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = "/jsp/")
 public class IndexController {
 
 	private String message = "Hello World";
 
-	@RequestMapping("/")
+	@RequestMapping(value = "/nav")
 	public String welcome(Map<String, Object> model) {
 		model.put("message", this.message);
-		return "index";
+		return "nav";
 	}
 
 }
