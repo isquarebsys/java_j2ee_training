1f640c801ef19afd8a2f84f4c6f23c040d668cce
isquarebsys <isquarebsys@gmail.com>
2018-05-25 09:26:40 +0530

    Test suite for both JUnit and TestNG

---
+++ TestingFrameworks/src/main/java/junit/JUnitTestSuiteSample1.java
@@ -0,0 +1,11 @@
+package junit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ JUnitAnnotationsAll.class, JUnitParemeterised.class, JUnitTestMethods.class })
+public class JUnitTestSuiteSample1 {
+
+}
